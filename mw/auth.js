15e8b83d4af45def69df08aec45f9dd4279301b4
Peerawat Poombua <peerawat@odds.team>
2020-05-03 16:00:43 +0700

    logout

--- mw/auth.js
+++ mw/auth.js
@@ -1,30 +1,31 @@
-const token = require("../token");
-const userDB = require("../db/user");
-
-const getToken = (req) => {
-  const prefix = "Bearer ";
-  const auth = req.headers["authorization"];
-  if (auth && auth.startsWith(prefix)) {
-    const tk = auth.substring(prefix.length);
-    return tk;
-  }
-};
+const Token = require("../token");
+const User = require("../models/user");
 
 const requiredAdmin = async (req, res, next) => {
-  const tk = getToken(req);
-  if (!tk) {
-    return res.sendStatus(401);
-  }
-  const claims = token.verifyToken(tk);
-  if (!claims) {
-    return res.sendStatus(401);
-  }
+  const claims = validateReq(req);
   if (claims.role !== "admin" || claims.type !== "access") {
     return res.sendStatus(401);
   }
-
   try {
-    const user = await userDB.getByID(claims.user_id);
+    const user = await User.findById(claims.userID).exec();
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    req.User = user;
+    next();
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(401);
+  }
+};
+
+const requiredStaff = async (req, res, next) => {
+  const claims = validateReq(req);
+  if (claims.role !== "staff" || claims.type !== "access") {
+    return res.sendStatus(401);
+  }
+  try {
+    const user = await User.findById(claims.userID).exec();
     if (!user) {
       return res.sendStatus(401);
     }
@@ -37,20 +38,12 @@ const requiredAdmin = async (req, res, next) => {
 };
 
 const requiredUser = async (req, res, next) => {
-  const tk = getToken(req);
-  if (!tk) {
-    return res.sendStatus(401);
-  }
-  const claims = token.verifyToken(tk);
-  if (!claims) {
-    return res.sendStatus(401);
-  }
+  const claims = validateReq(req);
   if (claims.type !== "access") {
     return res.sendStatus(401);
   }
-
   try {
-    const user = await userDB.getByID(claims.user_id);
+    const user = await User.findById(claims.userID).exec();
     if (!user) {
       return res.sendStatus(401);
     }
@@ -62,7 +55,29 @@ const requiredUser = async (req, res, next) => {
   }
 };
 
+const validateReq = (req) => {
+  const tk = getToken(req);
+  if (!tk) {
+    return res.sendStatus(401);
+  }
+  const claims = Token.verifyToken(tk);
+  if (!claims) {
+    return res.sendStatus(401);
+  }
+  return claims;
+};
+
+const getToken = (req) => {
+  const prefix = "Bearer ";
+  const auth = req.headers["authorization"];
+  if (auth && auth.startsWith(prefix)) {
+    const tk = auth.substring(prefix.length);
+    return tk;
+  }
+};
+
 module.exports = {
   requiredAdmin,
+  requiredStaff,
   requiredUser,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,72 +1,85 @@
+const bcrypt = require("bcrypt");
+const Token = require("../token");
+const Auth = require("../mw/auth");
 const User = require("../models/user");
 
 const init = (app) => {
-  // app.post("/login", async (req, res, next) => {
-  //   const { username, password } = req.body;
-  //   try {
-  //     let user = await userDB.getByUsername(username);
-  //     if (!user) {
-  //       return res.status(401).json({
-  //         message: "Username or Password is incorrect",
-  //       });
-  //     }
-  //     const valid = await bcrypt.compare(password, user.password);
-  //     if (!valid) {
-  //       return res.status(401).json({
-  //         message: "Username or Password is incorrect",
-  //       });
-  //     }
-  //     const refreshToken = token.generateRefreshToken({
-  //       user_id: user.id,
-  //       role: user.role,
-  //     });
-  //     user = { ...user, refreshToken };
-  //     user = await userDB.update(user);
-  //     const accessToken = token.generateAccessToken({
-  //       user_id: user.id,
-  //       role: user.role,
-  //     });
-  //     res.json({
-  //       data: {
-  //         username: user.username,
-  //         refreshToken,
-  //         accessToken,
-  //       },
-  //     });
-  //   } catch (e) {
-  //     next(e);
-  //   }
-  // });
-  // app.post("/sessions", async (req, res, next) => {
-  //   const { refreshToken } = req.body;
-  //   try {
-  //     const user = await userDB.getByRefreshToken(refreshToken);
-  //     if (!user) {
-  //       return res.sendStatus(401);
-  //     }
-  //     const accessToken = token.generateAccessToken({
-  //       user_id: user.id,
-  //       role: user.role,
-  //     });
-  //     res.json({
-  //       data: {
-  //         username: user.username,
-  //         accessToken,
-  //       },
-  //     });
-  //   } catch (e) {
-  //     next(e);
-  //   }
-  // });
-  // app.post("/logout", auth.requiredUser, async (req, res, next) => {
-  //   let user = req.User;
-  //   user.refreshToken = undefined;
-  //   try {
-  //     await userDB.update(user);
-  //   } catch (e) {
-  //     next(e);
-  //   }
-  // });
+  app.post("/login", async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+      const user = await User.findOne({ username }).exec();
+      if (!user) {
+        return res.status(401).json({
+          message: "Username or Password is incorrect",
+        });
+      }
+      const valid = await bcrypt.compare(password, user.password);
+      if (!valid) {
+        return res.status(401).json({
+          message: "Username or Password is incorrect",
+        });
+      }
+      const refreshToken = Token.generateRefreshToken({
+        userID: user.id,
+        role: user.role,
+      });
+      user.refreshToken = refreshToken;
+      await user.save();
+      const accessToken = Token.generateAccessToken({
+        userID: user.id,
+        role: user.role,
+      });
+      res.json({
+        data: {
+          name: user.name,
+          refreshToken,
+          accessToken,
+        },
+      });
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  app.get("/sessions", Auth.requiredUser, async (req, res, next) => {
+    res.json({
+      name: req.User.name,
+      refreshToken: req.User.refreshToken,
+    });
+  });
+
+  app.post("/sessions", async (req, res, next) => {
+    const { refreshToken } = req.body;
+    try {
+      const user = await User.findOne({ refreshToken }).exec();
+      if (!user) {
+        return res.sendStatus(401);
+      }
+      const accessToken = Token.generateAccessToken({
+        userID: user.id,
+        role: user.role,
+      });
+      res.json({
+        data: {
+          name: user.name,
+          accessToken,
+        },
+      });
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  app.post("/logout", Auth.requiredUser, async (req, res, next) => {
+    const user = req.User;
+    try {
+      user.refreshToken = undefined;
+      await user.save();
+      res.sendStatus(204);
+    } catch (e) {
+      next(e);
+    }
+  });
 };
 
 module.exports = {
